d435c2b8283ec285a0d51fbf4fa6a08779f29600
David Machek <59211556+dmachek@users.noreply.github.com>
2023-06-02 23:50:07 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,13 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         //Vytvoření turnaje, jediné parametry jsou počet týmů a počet disciplín
+        //Turnaj se po inicializaci pokusí o rozřazení
         Tournament tournament = new Tournament(12,12);
 
-        //Vytvoří rozpis zápasůna základě požadavků
-        tournament.generateTournament();
-
         // Zobrazí zbývající disciplíny všech týmů a proti komu již mají zápas
-        tournament.displayAllLeft();
+        // tournament.displayAllLeft();
+
+        //Log zápasů
+        // System.out.println(tournament.getLog());
 
     }
 }
